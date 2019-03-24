76f0f8cb6b81e436101cb6b612622763fd39870f
radocisar <radocisar@hotmail.com>
2019-03-24 16:36:38 -0400

    Updating logs path.

--- Raw_ticks/Open_str_wrt.vb
+++ Raw_ticks/Open_str_wrt.vb
@@ -7,7 +7,7 @@ Public Class Open_str_wrt
     Public Sub Open_str_wrt_method()
 
         'Dim str_wrt As StreamWriter = New StreamWriter("C:\Raw_Data\Logging _Auto_Trading\log.txt", True)
-        str_wrt = New StreamWriter("C:\Raw_Data\Logging _Auto_Trading\log.txt", True)
+        str_wrt = New StreamWriter("D:\Auto_Trading_Logs\log.txt", True)
 
     End Sub
 
